b23c6fb0a18a8ee14daefa333c1f245cf99b47a7
laiquanghai2k6 <yortvrluer@gmail.com>
2025-05-23 20:37:51 +0700

    payment

--- client/src/app/payment-success/page.tsx
+++ client/src/app/payment-success/page.tsx
@@ -1,38 +1,16 @@
 'use client'
 
+import PaymentSuccess from "@/components/Payment/PaymentSuccess";
 import { LoadingType, setLoading } from "@/slice/loadingSlice";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-const PaymentSuccess = () => {
-    const searchParams = useSearchParams();
-    const dispatch = useDispatch()
-    const router = useRouter()
-     useEffect(() => {
-    const fetchStatus = async () => {
-      const orderId = searchParams.get('orderId');
-      if (orderId) {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/order/${orderId}`);
-        if(res.data.status == 'paid')
-        dispatch(setLoading({active:true,text:'Thanh toán thành công',type:LoadingType.SUCCESS}))
-        else if(res.data.status == 'pending')
-        dispatch(setLoading({active:true,text:'Thanh toán thành công',type:LoadingType.SUCCESS}))
-        else 
-        dispatch(setLoading({active:true,text:'Thanh toán thất bại',type:LoadingType.ERROR}))
-
-      }
-    };
-        router.push('/')
-
-    fetchStatus();
-  }, []);
-
-    return ( 
-        <></>
-     );
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentSuccess />
+    </Suspense>
+  );
 }
-
- 
-export default PaymentSuccess ;
---
+++ client/src/components/Payment/PaymentSuccess.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { LoadingType, setLoading } from "@/slice/loadingSlice";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+
+
+const PaymentSuccess = () => {
+    const searchParams = useSearchParams();
+    const dispatch = useDispatch()
+    const router = useRouter()
+     useEffect(() => {
+    const fetchStatus = async () => {
+      const orderId = searchParams.get('orderId');
+      if (orderId) {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/order/${orderId}`);
+        if(res.data.status == 'paid')
+        dispatch(setLoading({active:true,text:'Thanh toán thành công',type:LoadingType.SUCCESS}))
+        else if(res.data.status == 'pending')
+        dispatch(setLoading({active:true,text:'Thanh toán thành công',type:LoadingType.SUCCESS}))
+        else 
+        dispatch(setLoading({active:true,text:'Thanh toán thất bại',type:LoadingType.ERROR}))
+
+      }
+    };
+        router.push('/')
+
+    fetchStatus();
+  }, []);
+
+    return ( 
+        <></>
+     );
+}
+
+ 
+export default PaymentSuccess ;
