1664eca6e0cfc649fe8fecb375cc0673c8b650da
laiquanghai2k6 <yortvrluer@gmail.com>
2025-05-24 15:49:57 +0700

    top nav

--- client/src/components/NavBar/TopNav.tsx
+++ client/src/components/NavBar/TopNav.tsx
@@ -12,58 +12,61 @@ import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { useRouter } from "next/navigation";
 import { LoadingType, setLoading } from "@/slice/loadingSlice";
-const TopNav = ({Loading}:{Loading:Function}) => {
+const TopNav = ({ Loading }: { Loading: Function }) => {
     const router = useRouter()
-    const dispatch =useDispatch()
-    const [isPending,startTransition] = useTransition()
-    const currentUser = useSelector((state:RootState)=>{
-        return{
-            name:state.user.user.email.split('@')[0],
-            image:state.user.user.image
+    const dispatch = useDispatch()
+    const [isPending, startTransition] = useTransition()
+    const currentUser = useSelector((state: RootState) => {
+        return {
+            name: state.user.user.email.split('@')[0],
+            image: state.user.user.image
         }
-    },shallowEqual)
-    const [openSettingModal,setOpenSettingModal] = useState(false)
-     const closeModalSetting = useCallback(()=>{
+    }, shallowEqual)
+    const [openSettingModal, setOpenSettingModal] = useState(false)
+    const closeModalSetting = useCallback(() => {
         setOpenSettingModal(false)
-      },[])
-      useEffect(()=>{
-        if(isPending) Loading(true)
-            else Loading(false)
-      },[isPending])
-      const ComingSoon = ()=>{
-        dispatch(setLoading({active:true,text:'Chức năng sẽ được cập nhật',type:LoadingType.PENDING}))
-      }
+    }, [])
+    useEffect(() => {
+        if (isPending) Loading(true)
+        else Loading(false)
+    }, [isPending])
+    const ComingSoon = () => {
+        dispatch(setLoading({ active: true, text: 'Chức năng sẽ được cập nhật', type: LoadingType.PENDING }))
+    }
+    const handleToggleCartModal = () => {
+        setOpenSettingModal((prev) => !prev);
+    };
     return (
-<>
-        <div className="flex flex-row justify-between max-md:pb-3 max-md:pt-1 ">
-            <div className="flex flex-row space-x-4 max-lg:hidden ">
-                <ItemText text="Kênh bán hàng" onClick={()=>ComingSoon()}/>
-                <ItemText text="Tải ứng dụng" onClick={()=>ComingSoon()} />
-                <ItemText text="Kết nối" onClick={()=>ComingSoon()} type="right" icon={FacebookIcon}  />
+        <>
+            <div className="flex flex-row justify-between max-md:pb-3 max-md:pt-1 ">
+                <div className="flex flex-row space-x-4 max-lg:hidden ">
+                    <ItemText text="Kênh bán hàng" onClick={() => ComingSoon()} />
+                    <ItemText text="Tải ứng dụng" onClick={() => ComingSoon()} />
+                    <ItemText text="Kết nối" onClick={() => ComingSoon()} type="right" icon={FacebookIcon} />
+                </div>
+                <div className="flex flex-row space-x-6 justify-center max-md:space-x-4 max-lg:justify-between max-lg:w-full ">
+                    <ItemText onClick={() => ComingSoon()} extraClass="max-md:text-[11px]" text="Thông báo" type="left" icon={Notification} />
+                    <ItemText onClick={() => ComingSoon()} extraClass="max-md:text-[11px]" text="Tải ứng dụng" type="left" icon={Question} />
+                    <ItemText onClick={() => ComingSoon()} extraClass="max-md:text-[11px]" text="Kết nối" type="left" icon={Global} />
+                    {currentUser.name != '' ? (
+
+                        <ItemText extraClass="relative" image={currentUser.image} onClick={()=>handleToggleCartModal()} isOpa={false} text={currentUser.name} type="left" icon={Default} isImage={true}  onMouseEnter={() => setOpenSettingModal(true)} onMouseLeave={() => setOpenSettingModal(false)} >
+                            <ModalSetting Loading={Loading} openSettingModal={openSettingModal} closeModalSetting={closeModalSetting} />
+
+                        </ItemText>
+                    ) : (
+                        <ItemText extraClass="max-md:text-[11px]" text="Đăng nhập" onClick={() => {
+                            startTransition(() => {
+
+                                router.push('/login')
+                            })
+                        }}
+                        />
+                    )}
+                </div>
+
             </div>
-            <div className="flex flex-row space-x-6 justify-center max-md:space-x-4 max-lg:justify-between max-lg:w-full ">
-                <ItemText onClick={()=>ComingSoon()}  extraClass="max-md:text-[11px]" text="Thông báo" type="left" icon={Notification} />
-                <ItemText onClick={()=>ComingSoon()}  extraClass="max-md:text-[11px]" text="Tải ứng dụng" type="left" icon={Question} />
-                <ItemText onClick={()=>ComingSoon()} extraClass="max-md:text-[11px]"  text="Kết nối" type="left" icon={Global} />
-                {currentUser.name != '' ? (
-
-                <ItemText extraClass="relative" image={currentUser.image} isOpa={false} text={currentUser.name} type="left" icon={Default} isImage={true} onMouseEnter={()=>setOpenSettingModal(true)} onMouseLeave={()=>setOpenSettingModal(false)} >
-                 <ModalSetting Loading={Loading}  openSettingModal={openSettingModal}closeModalSetting={closeModalSetting} />
-                    
-                </ItemText>
-                ):(
-                    <ItemText extraClass="max-md:text-[11px]"  text="Đăng nhập" onClick={()=>{
-                        startTransition(()=>{
-
-                            router.push('/login')
-                        })
-                    }}
-                         />
-                )}
-            </div>
-
-        </div>
-</>
+        </>
 
     );
 }
