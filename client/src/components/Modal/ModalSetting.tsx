ecd63e93d5d036d2bbbcd65d72a59c83a7320556
laiquanghai2k6 <yortvrluer@gmail.com>
2025-05-24 16:01:58 +0700

    bg white

--- client/src/components/Modal/ModalSetting.tsx
+++ client/src/components/Modal/ModalSetting.tsx
@@ -88,19 +88,19 @@ setIsLoggingOut(false)
 
                     <div className={`md:shadow-[0px_0px_7px_rgba(0,0,0,0.2)] shadow-lg bg-white/100 !bg-white w-40  border border-gray-300 transition-all absolute origin-top-right ${openSettingModal ? 'appearCart' : 'disappearCart'} z-[22000]  w-40 h-fit min-h-15 top-[100%] right-0 bg-white`}>
                         <div className="w-full h-full flex flex-col">
-                            <div onClick={() => GoToAccount()} className="hover:bg-gray-300 max-md:active:bg-gray-300 p-4 flex items-center group  ">
+                            <div onClick={() => GoToAccount()} className="hover:bg-gray-300 bg-white max-md:active:bg-gray-300 p-4 flex items-center group  ">
                                 <p className=" text-[15px] text-black group-hover:text-[#00bfa5] max-md:group-active:text-[#00bfa5]">Tài khoản của tôi</p>
                             </div>
-                            <div onClick={() => GoToHistory()} className="hover:bg-gray-300 max-md:active:bg-gray-300 p-4 flex items-center group ">
+                            <div onClick={() => GoToHistory()} className="hover:bg-gray-300 bg-white max-md:active:bg-gray-300 p-4 flex items-center group ">
 
-                                <p className="group-hover:text-[#00bfa5] max-md:group-active:text-[#00bfa5] text-[15px] text-black">Lịch sử mua</p>
+                                <p className="group-hover:text-[#00bfa5] max-md:group-active:text-[#00bfa5]  text-[15px] text-black">Lịch sử mua</p>
                             </div>
 
-                            <div onClick={() => GoToEditMode()} className="hover:bg-gray-300 max-md:active:bg-gray-300 p-4 flex items-center group ">
+                            <div onClick={() => GoToEditMode()} className="hover:bg-gray-300 bg-white max-md:active:bg-gray-300 p-4 flex items-center group ">
 
                                 <p className="group-hover:text-[#00bfa5] max-md:group-active:text-[#00bfa5] text-[15px] text-black"> Chỉnh gian hàng</p>
                             </div>
-                            <div onClick={() => LogOut()} className="hover:bg-gray-300 p-4 flex max-md:active:bg-gray-300 items-center group">
+                            <div onClick={() => LogOut()} className="hover:bg-gray-300 p-4 flex bg-white max-md:active:bg-gray-300 items-center group">
 
                                 <p className="group-hover:text-[#00bfa5] max-md:group-active:text-[#00bfa5] text-[15px] text-black">Đăng xuất</p>
                             </div>
